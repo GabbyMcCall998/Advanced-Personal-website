f7e565704a7549eab32d68d9780398a03d405b92
GabbyMcCall998 <103126216+GabbyMcCall998@users.noreply.github.com>
2023-10-05 16:11:39 +0000

    Added node server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.static('public'));
+
+
+
+app.get("/",function(req,res){
+res.sendFile(__dirname + "/index - Copy.html");
+});
+
+
+app.listen(5000,function(){
+    console.log("The app is running on port 5000");
+});
+
